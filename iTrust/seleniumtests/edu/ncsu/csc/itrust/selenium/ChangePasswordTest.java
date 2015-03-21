9dfa8e11ad111c0991e0377828846b4aaabcf0bd
kjcommon <kjcommon@ncsu.edu>
2015-03-21 15:21:27 -0400

    All Tests in ChangePasswordTest converted.

--- iTrust/seleniumtests/edu/ncsu/csc/itrust/selenium/ChangePasswordTest.java
+++ iTrust/seleniumtests/edu/ncsu/csc/itrust/selenium/ChangePasswordTest.java
@@ -39,7 +39,7 @@ public class ChangePasswordTest extends iTrustSeleniumTest {
 		List<WebElement> links = driver.findElements(By.tagName("a"));
 
 		int count = 0;
-		//get the second link with General Checkup-5
+
 		for(int i = 0; i < links.size(); i++) {
 			if(links.get(i).getAttribute("href").contains("changePassword"))
 			{
@@ -70,160 +70,209 @@ public class ChangePasswordTest extends iTrustSeleniumTest {
 		assertEquals("iTrust - Patient Home", driver.getTitle());
 	}
 
-/*	public void testChangePassword_Acceptance_Long() throws Exception {
+	public void testChangePassword_Acceptance_Long() throws Exception {
 		//Patient1 logs into iTrust
-		WebConversation wc = login("1", "pw");
-		WebResponse wr = wc.getCurrentPage();
-		assertEquals("iTrust - Patient Home", wr.getTitle());
+		driver = (HtmlUnitDriver)login("1", "pw");
+		assertTrue(driver.getTitle().contains("iTrust - Patient Home"));  
 		
 		//User goes to change password
-		wr = wr.getLinkWith("Change Password").click();
+		List<WebElement> links = driver.findElements(By.tagName("a"));
+
+		int count = 0;
+
+		for(int i = 0; i < links.size(); i++) {
+			if(links.get(i).getAttribute("href").contains("changePassword"))
+			{
+				count = i;
+				break;
+			}
+		}
+		
+		links.get(count).click();
 		
 		//User types in their current, new, and confirm passwords
-		WebForm wf = wr.getFormWithID("mainForm");
-		wf.setParameter("oldPass", "pw");
-		wf.setParameter("newPass", "pass12345abcde");
-		wf.setParameter("confirmPass", "pass12345abcde");
-		
+        driver.findElement(By.name("oldPass")).sendKeys("pw");
+        driver.findElement(By.name("newPass")).sendKeys("pass12345abcde");
+        driver.findElement(By.name("confirmPass")).sendKeys("pass12345abcde");
+        driver.findElement(By.name("oldPass")).submit();
+        
 		//User submits password change. Change logged
-		wf.submit(wf.getSubmitButtons()[0]);
-		wr = wc.getCurrentPage();
-		assertTrue(wr.getText().contains("Password Changed"));
+        assertTrue(driver.getPageSource().contains("Password Changed"));
 		assertLogged(TransactionType.PASSWORD_CHANGE, 1L, 0, "");
 		
 		//User logs out
-		wr = wr.getLinkWith("Logout").click();
+		links = driver.findElements(By.tagName("a"));
+		links.get(count - 1).click();	
 		
 		//User can't log in with old password, but can with new one
-		wc = login("1", "pw");
-		assertEquals("iTrust - Login", wc.getCurrentPage().getTitle());
-		wc = login("1", "pass12345abcde");
-		assertEquals("iTrust - Patient Home", wc.getCurrentPage().getTitle());
+		driver = (HtmlUnitDriver)login("1", "pw");
+		assertEquals("iTrust - Login", driver.getTitle());
+		driver = (HtmlUnitDriver)login("1", "pass12345abcde");
+		assertEquals("iTrust - Patient Home", driver.getTitle());
 	}
 	
 	public void testChangePassword_Invalid_Length() throws Exception {
 		//Patient1 logs into iTrust
-		WebConversation wc = login("1", "pw");
-		WebResponse wr = wc.getCurrentPage();
-		assertEquals("iTrust - Patient Home", wr.getTitle());
+		driver = (HtmlUnitDriver)login("1", "pw");
+		assertTrue(driver.getTitle().contains("iTrust - Patient Home"));  
 		
 		//User goes to change password
-		wr = wr.getLinkWith("Change Password").click();
+		List<WebElement> links = driver.findElements(By.tagName("a"));
+
+		int count = 0;
+
+		for(int i = 0; i < links.size(); i++) {
+			if(links.get(i).getAttribute("href").contains("changePassword"))
+			{
+				count = i;
+				break;
+			}
+		}
+		
+		links.get(count).click();
 		
 		//User types in their current, new, and confirm passwords
-		WebForm wf = wr.getFormWithID("mainForm");
-		wf.setParameter("oldPass", "pw");
-		wf.setParameter("newPass", "pas1");
-		wf.setParameter("confirmPass", "pas1");
+        driver.findElement(By.name("oldPass")).sendKeys("pw");
+        driver.findElement(By.name("newPass")).sendKeys("pas1");
+        driver.findElement(By.name("confirmPass")).sendKeys("pas1");
+        driver.findElement(By.name("oldPass")).submit();
 		
 		//User submits password change. Change logged
-		wf.submit(wf.getSubmitButtons()[0]);
-		wr = wc.getCurrentPage();
-		assertTrue(wr.getText().contains("Invalid password"));
+        assertTrue(driver.getPageSource().contains("Invalid password"));
 		assertLogged(TransactionType.PASSWORD_CHANGE_FAILED, 1L, 0, "");
 		
 		//User logs out
-		wr = wr.getLinkWith("Logout").click();
+		links = driver.findElements(By.tagName("a"));
+		links.get(count - 1).click();	
 		
 		//User can log in with old password, but can't with new one
-		wc = login("1", "pas1");
-		assertEquals("iTrust - Login", wc.getCurrentPage().getTitle());
-		wc = login("1", "pw");
-		assertEquals("iTrust - Patient Home", wc.getCurrentPage().getTitle());
+		driver = (HtmlUnitDriver)login("1", "pas1");
+		assertTrue(driver.getTitle().contains("iTrust - Login"));  
+		driver = (HtmlUnitDriver)login("1", "pw");
+		assertTrue(driver.getTitle().contains("iTrust - Patient Home"));  
 	}
 	
 	public void testChangePassword_Invalid_Characters() throws Exception {
 		//Patient1 logs into iTrust
-		WebConversation wc = login("1", "pw");
-		WebResponse wr = wc.getCurrentPage();
-		assertEquals("iTrust - Patient Home", wr.getTitle());
+		driver = (HtmlUnitDriver)login("1", "pw");
+		assertTrue(driver.getTitle().contains("iTrust - Patient Home"));  
 		
 		//User goes to change password
-		wr = wr.getLinkWith("Change Password").click();
+		List<WebElement> links = driver.findElements(By.tagName("a"));
+
+		int count = 0;
+
+		for(int i = 0; i < links.size(); i++) {
+			if(links.get(i).getAttribute("href").contains("changePassword"))
+			{
+				count = i;
+				break;
+			}
+		}
 		
+		links.get(count).click();
+
 		//User types in their current, new, and confirm passwords
-		WebForm wf = wr.getFormWithID("mainForm");
-		wf.setParameter("oldPass", "pw");
-		wf.setParameter("newPass", "password");
-		wf.setParameter("confirmPass", "password");
+        driver.findElement(By.name("oldPass")).sendKeys("pw");
+        driver.findElement(By.name("newPass")).sendKeys("password");
+        driver.findElement(By.name("confirmPass")).sendKeys("password");
+        driver.findElement(By.name("oldPass")).submit();
 		
 		//User submits password change. Change logged
-		wf.submit(wf.getSubmitButtons()[0]);
-		wr = wc.getCurrentPage();
-		assertTrue(wr.getText().contains("Invalid password"));
+        assertTrue(driver.getPageSource().contains("Invalid password"));
 		assertLogged(TransactionType.PASSWORD_CHANGE_FAILED, 1L, 0, "");
 		
 		//User logs out
-		wr = wr.getLinkWith("Logout").click();
+		links = driver.findElements(By.tagName("a"));
+		links.get(count - 1).click();	
 		
 		//User can log in with old password, but can't with new one
-		wc = login("1", "password");
-		assertEquals("iTrust - Login", wc.getCurrentPage().getTitle());
-		wc = login("1", "pw");
-		assertEquals("iTrust - Patient Home", wc.getCurrentPage().getTitle());
+		driver = (HtmlUnitDriver)login("1", "password");
+		assertTrue(driver.getTitle().contains("iTrust - Login"));  
+		driver = (HtmlUnitDriver)login("1", "pw");
+		assertTrue(driver.getTitle().contains("iTrust - Patient Home"));  
 	}
-	
+
 	public void testChangePassword_Failed_Confirmation() throws Exception {
 		//Patient1 logs into iTrust
-		WebConversation wc = login("1", "pw");
-		WebResponse wr = wc.getCurrentPage();
-		assertEquals("iTrust - Patient Home", wr.getTitle());
+		driver = (HtmlUnitDriver)login("1", "pw");
+		assertTrue(driver.getTitle().contains("iTrust - Patient Home"));  
 		
 		//User goes to change password
-		wr = wr.getLinkWith("Change Password").click();
+		List<WebElement> links = driver.findElements(By.tagName("a"));
+
+		int count = 0;
+
+		for(int i = 0; i < links.size(); i++) {
+			if(links.get(i).getAttribute("href").contains("changePassword"))
+			{
+				count = i;
+				break;
+			}
+		}
+		
+		links.get(count).click();
 		
 		//User types in their current, new, and confirm passwords
-		WebForm wf = wr.getFormWithID("mainForm");
-		wf.setParameter("oldPass", "pw");
-		wf.setParameter("newPass", "pass1");
-		wf.setParameter("confirmPass", "pass2");
+        driver.findElement(By.name("oldPass")).sendKeys("pw");
+        driver.findElement(By.name("newPass")).sendKeys("pass1");
+        driver.findElement(By.name("confirmPass")).sendKeys("pass2");
+        driver.findElement(By.name("oldPass")).submit();
 		
 		//User submits password change. Change logged
-		wf.submit(wf.getSubmitButtons()[0]);
-		wr = wc.getCurrentPage();
-		assertTrue(wr.getText().contains("Invalid password"));
+        assertTrue(driver.getPageSource().contains("Invalid password"));
 		assertLogged(TransactionType.PASSWORD_CHANGE_FAILED, 1L, 0, "");
 		
 		//User logs out
-		wr = wr.getLinkWith("Logout").click();
+		links = driver.findElements(By.tagName("a"));
+		links.get(count - 1).click();	
 		
 		//User can log in with old password, but can't with new one
-		wc = login("1", "pas1");
-		assertEquals("iTrust - Login", wc.getCurrentPage().getTitle());
-		wc = login("1", "pw");
-		assertEquals("iTrust - Patient Home", wc.getCurrentPage().getTitle());
+		driver = (HtmlUnitDriver)login("1", "pas1");
+		assertTrue(driver.getTitle().contains("iTrust - Login"));  
+		driver = (HtmlUnitDriver)login("1", "pw");
+		assertTrue(driver.getTitle().contains("iTrust - Patient Home"));  
 	}
-	
+
 	public void testChangePassword_Invalid_Password() throws Exception {
 		//Patient1 logs into iTrust
-		WebConversation wc = login("1", "pw");
-		WebResponse wr = wc.getCurrentPage();
-		assertEquals("iTrust - Patient Home", wr.getTitle());
+		driver = (HtmlUnitDriver)login("1", "pw");
+		assertTrue(driver.getTitle().contains("iTrust - Patient Home"));  
 		
 		//User goes to change password
-		wr = wr.getLinkWith("Change Password").click();
+		List<WebElement> links = driver.findElements(By.tagName("a"));
+
+		int count = 0;
+
+		for(int i = 0; i < links.size(); i++) {
+			if(links.get(i).getAttribute("href").contains("changePassword"))
+			{
+				count = i;
+				break;
+			}
+		}
 		
+		links.get(count).click();
+
 		//User types in their current, new, and confirm passwords
-		WebForm wf = wr.getFormWithID("mainForm");
-		wf.setParameter("oldPass", "password");
-		wf.setParameter("newPass", "pass1");
-		wf.setParameter("confirmPass", "pass1");
+        driver.findElement(By.name("oldPass")).sendKeys("password");
+        driver.findElement(By.name("newPass")).sendKeys("pass1");
+        driver.findElement(By.name("confirmPass")).sendKeys("pass1");
+        driver.findElement(By.name("oldPass")).submit();
 		
 		//User submits password change. Change logged
-		wf.submit(wf.getSubmitButtons()[0]);
-		wr = wc.getCurrentPage();
-		assertTrue(wr.getText().contains("Invalid password"));
+        assertTrue(driver.getPageSource().contains("Invalid password"));
 		assertLogged(TransactionType.PASSWORD_CHANGE_FAILED, 1L, 0, "");
 		
 		//User logs out
-		wr = wr.getLinkWith("Logout").click();
+		links = driver.findElements(By.tagName("a"));
+		links.get(count - 1).click();	
 		
 		//User can log in with old password, but can't with new one
-		wc = login("1", "pass1");
-		assertEquals("iTrust - Login", wc.getCurrentPage().getTitle());
-		wc = login("1", "pw");
-		assertEquals("iTrust - Patient Home", wc.getCurrentPage().getTitle());
+		driver = (HtmlUnitDriver)login("1", "pass1");
+		assertTrue(driver.getTitle().contains("iTrust - Login"));  
+		driver = (HtmlUnitDriver)login("1", "pw");
+		assertTrue(driver.getTitle().contains("iTrust - Patient Home"));  
 	}
-	*/
 
 }
