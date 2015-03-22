7cbbb09eadc21460996307c17c1c4d61556a1c54
Ryan Upham <rlupham@ncsu.edu>
2015-03-22 15:41:39 -0400

    Added CreateHCPTest and CreateHCPSpecTest.

--- iTrust/seleniumtests/edu/ncsu/csc/itrust/selenium/BasicHealthInfoTest.java
+++ iTrust/seleniumtests/edu/ncsu/csc/itrust/selenium/BasicHealthInfoTest.java
@@ -1,20 +1,22 @@
 package edu.ncsu.csc.itrust.selenium;
 
 import org.openqa.selenium.*;
+
+import edu.ncsu.csc.itrust.enums.TransactionType;
  
 public class BasicHealthInfoTest extends iTrustSeleniumTest {
  
 	@Override
 	protected void setUp() throws Exception{
 		super.setUp();
+		gen.clearAllTables();
+		gen.standardData();
 	}
 	
 	public void testBasicHealthViewed() throws Exception{
 		WebDriver driver = login("9000000000", "pw");
 		
-		System.out.println(driver.getTitle());
-		
-		//assertLogged(TransactionType.HOME_VIEW, 9000000000L, 0L, ""); //still have to implement this
+		assertLogged(TransactionType.HOME_VIEW, 9000000000L, 0L, "");
 		
 		driver.findElement(By.linkText("Basic Health Information")).click();
 		driver.findElement(By.name("UID_PATIENTID")).sendKeys("2");
@@ -34,44 +36,34 @@ public class BasicHealthInfoTest extends iTrustSeleniumTest {
 		assertTrue(driver.getPageSource().contains("viewed your health records history today at"));
 	}
 	
-	/*public void testBasicHealthSmokingStatus() throws Exception{
-		WebConversation wc = login("9000000000", "pw");	
-		WebResponse wr = wc.getCurrentPage();
+	public void testBasicHealthSmokingStatus() throws Exception {
+		WebDriver driver = login("9000000000", "pw");
+		
 		assertLogged(TransactionType.HOME_VIEW, 9000000000L, 0L, "");
 		
-		//Click Document Office Visit
-		wr = wr.getLinkWith("Document Office Visit").click();
-		assertEquals(ADDRESS + "auth/getPatientID.jsp?forward=/iTrust/auth/hcp-uap/documentOfficeVisit.jsp", wr.getURL().toString());
-		//Choose patient 2
-		WebForm patientForm = wr.getForms()[0];
-		patientForm.getScriptableObject().setParameterValue("UID_PATIENTID", "2");
-		patientForm.getButtons()[1].click();
-		wr = wc.getCurrentPage();
-		assertEquals(ADDRESS + "auth/hcp-uap/documentOfficeVisit.jsp", wr.getURL().toString());
+		driver.findElement(By.linkText("Document Office Visit")).click();
+
+		assertEquals(ADDRESS + "auth/getPatientID.jsp?forward=/iTrust/auth/hcp-uap/documentOfficeVisit.jsp", driver.getCurrentUrl());
 		
-		//Click Yes, Document Office Visit
-		WebForm form = wr.getForms()[0];
-		form.getButtons()[0].click();
-		//Verify Edit Office Visit page
-		wr = wc.getCurrentPage();
-		assertEquals("iTrust - Document Office Visit", wr.getTitle());
+		driver.findElement(By.name("UID_PATIENTID")).sendKeys("2");
+		driver.findElement(By.cssSelector("input[value='2']")).submit();
 		
-		//Add a new office visit
-		form = wr.getFormWithID("mainForm");
-		//Click the create button
-		form.getButtonWithID("update").click();
+		assertEquals(ADDRESS + "auth/hcp-uap/documentOfficeVisit.jsp", driver.getCurrentUrl());
 		
-		//Verify "Information Successfully Updated" message
-		wr = wc.getCurrentPage();
-		assertTrue(wr.getText().contains("Information Successfully Updated"));
+		driver.findElement(By.cssSelector("input[value='Yes, Document Office Visit']")).submit();
+		
+		assertEquals("iTrust - Document Office Visit", driver.getTitle());
+		
+		driver.findElement(By.name("update")).submit();
+		
+		assertTrue(driver.getPageSource().contains("Information Successfully Updated"));
 		assertLogged(TransactionType.OFFICE_VISIT_CREATE, 9000000000L, 2L, "");
 		
-		//Check patient smoking status options
-		assertTrue(wr.getText().contains("1 - Current every day smoker"));
-		assertTrue(wr.getText().contains("2 - Current some day smoker"));
-		assertTrue(wr.getText().contains("3 - Former smoker"));
-		assertTrue(wr.getText().contains("4 - Never smoker"));
-		assertTrue(wr.getText().contains("5 - Smoker, current status unknown"));
-		assertTrue(wr.getText().contains("9 - Unknown if ever smoked"));
-	}*/
+		assertTrue(driver.getPageSource().contains("1 - Current every day smoker"));
+		assertTrue(driver.getPageSource().contains("2 - Current some day smoker"));
+		assertTrue(driver.getPageSource().contains("3 - Former smoker"));
+		assertTrue(driver.getPageSource().contains("4 - Never smoker"));
+		assertTrue(driver.getPageSource().contains("5 - Smoker, current status unknown"));
+		assertTrue(driver.getPageSource().contains("9 - Unknown if ever smoked"));
+	}
 }
