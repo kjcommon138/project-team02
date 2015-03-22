ea9d5f7b8de0a6253e6f4f84f315b582c7c90a1b
Michael Goff <magoff2@ncsu.edu>
2015-03-22 18:10:14 -0400

    Resolved #13 AuditPatientTest with passing tests

--- iTrust/seleniumtests/edu/ncsu/csc/itrust/selenium/AuditPatientTest.java
+++ iTrust/seleniumtests/edu/ncsu/csc/itrust/selenium/AuditPatientTest.java
@@ -1,23 +1,9 @@
 package edu.ncsu.csc.itrust.selenium;
 
-import static org.junit.Assert.*;
-
-import java.util.concurrent.TimeUnit;
-
-import junit.framework.TestCase;
-
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-
-import com.meterware.httpunit.WebConversation;
-import com.meterware.httpunit.WebForm;
-import com.meterware.httpunit.WebResponse;
-
-import edu.ncsu.csc.itrust.datagenerators.TestDataGenerator;
 
 public class AuditPatientTest extends iTrustSeleniumTest {
 
@@ -36,76 +22,75 @@ public class AuditPatientTest extends iTrustSeleniumTest {
 		driver.findElement(By.linkText("Audit Patients")).click();
 		assertEquals(driver.getTitle(), "iTrust - Please Select a Patient");
 		
-		
+		//searching for patient 2
 		driver.findElement(By.name("UID_PATIENTID")).sendKeys("2");
-		driver.findElement(By.cssSelector("input[value='Deactivate']")).submit();
+		driver.findElement(By.name("UID_PATIENTID")).submit();
 		assertEquals(driver.getTitle(), "iTrust - Audit Page (UC62)");
 		
-//		WebForm patientForm = wr.getForms()[0];
-//		patientForm.getScriptableObject().setParameterValue("UID_PATIENTID", "2");
-//		patientForm.getButtons()[1].click();
-//		wr = wc.getCurrentPage();
-//		assertEquals(ADDRESS + "auth/hcp/auditPage.jsp", wr.getURL().toString());
-//		
-//		WebForm editPatientForm = wr.getForms()[0];
-//		editPatientForm.setParameter("understand", "I UNDERSTAND");
-//		editPatientForm.getButtons()[0].click();
-//		wr = wc.getCurrentPage();
-//		assertTrue(wr.getText().contains("Patient Successfully Deactivated"));
+		//typing out I understand
+		driver.findElement(By.name("understand")).sendKeys("I UNDERSTAND");
+		driver.findElement(By.name("understand")).submit();
+		
+		//asserting deletion
+		assertEquals("Patient Successfully Deactivated", driver.findElement(By.className("iTrustMessage")).getText());
 	}
 	
-//	public void testHCPDeactivatePatientWrongConfirmation() throws Exception{
-//		WebConversation wc = login("9000000000", "pw");	
-//		WebResponse wr = wc.getCurrentPage();
-//		
-//		wr = wr.getLinkWith("Audit Patients").click();
-//		WebForm patientForm = wr.getForms()[0];
-//		patientForm.getScriptableObject().setParameterValue("UID_PATIENTID", "2");
-//		patientForm.getButtons()[1].click();
-//		wr = wc.getCurrentPage();
-//		assertEquals(ADDRESS + "auth/hcp/auditPage.jsp", wr.getURL().toString());
-//		
-//		WebForm editPatientForm = wr.getForms()[0];
-//		editPatientForm.setParameter("understand", "iunderstand");
-//		editPatientForm.getButtons()[0].click();
-//		wr = wc.getCurrentPage();
-//		assertTrue(wr.getText().contains("You must type \"I UNDERSTAND\" in the textbox."));
-//	}
-//	
-//	public void testHCPActivatePatient() throws Exception {
-//		WebConversation wc = login("9000000000", "pw");	
-//		WebResponse wr = wc.getCurrentPage();
-//		
-//		wr = wr.getLinkWith("Audit Patients").click();
-//		WebForm patientForm = wr.getForms()[0];
-//		patientForm.getScriptableObject().setParameterValue("UID_PATIENTID", "314159");
-//		patientForm.getButtons()[1].click();
-//		wr = wc.getCurrentPage();
-//		assertEquals(ADDRESS + "auth/hcp/auditPage.jsp", wr.getURL().toString());
-//		
-//		WebForm editPatientForm = wr.getForms()[0];
-//		editPatientForm.setParameter("understand", "I UNDERSTAND");
-//		editPatientForm.getButtons()[0].click();
-//		wr = wc.getCurrentPage();
-//		assertTrue(wr.getText().contains("Patient Successfully Activated"));
-//	}
-//	
-//	public void testHCPActivatePatientWrongConfirmation() throws Exception {
-//		WebConversation wc = login("9000000000", "pw");	
-//		WebResponse wr = wc.getCurrentPage();
-//		
-//		wr = wr.getLinkWith("Audit Patients").click();
-//		WebForm patientForm = wr.getForms()[0];
-//		patientForm.getScriptableObject().setParameterValue("UID_PATIENTID", "314159");
-//		patientForm.getButtons()[1].click();
-//		wr = wc.getCurrentPage();
-//		assertEquals(ADDRESS + "auth/hcp/auditPage.jsp", wr.getURL().toString());
-//		
-//		WebForm editPatientForm = wr.getForms()[0];
-//		editPatientForm.setParameter("understand", "WOAH");
-//		editPatientForm.getButtons()[0].click();
-//		wr = wc.getCurrentPage();
-//		assertTrue(wr.getText().contains("You must type \"I UNDERSTAND\" in the textbox."));
-//	}
-
+	public void testHCPDeactivatePatientWrongConfirmation() throws Exception {
+		WebDriver driver = login("9000000000", "pw");
+		
+		driver.findElement(By.linkText("Audit Patients")).click();
+		assertEquals(driver.getTitle(), "iTrust - Please Select a Patient");
+		
+		//searching for patient 2
+		driver.findElement(By.name("UID_PATIENTID")).sendKeys("2");
+		driver.findElement(By.name("UID_PATIENTID")).submit();
+		assertEquals(driver.getTitle(), "iTrust - Audit Page (UC62)");
+		
+		//typing out I understand
+		driver.findElement(By.name("understand")).sendKeys("iunderstand");
+		driver.findElement(By.name("understand")).submit();
+		
+		//asserting deletion
+		assertEquals("You must type \"I UNDERSTAND\" in the textbox.", driver.findElement(By.className("iTrustError")).getText());
+	}
+	
+	public void testHCPActivatePatient() throws Exception {
+		WebDriver driver = login("9000000000", "pw");
+		
+		driver.findElement(By.linkText("Audit Patients")).click();
+		assertEquals(driver.getTitle(), "iTrust - Please Select a Patient");
+		
+		//searching for patient 314159
+		driver.findElement(By.id("allowDeactivated")).click();
+		driver.findElement(By.name("UID_PATIENTID")).sendKeys("314159");
+		driver.findElement(By.name("UID_PATIENTID")).submit();
+		assertEquals(driver.getTitle(), "iTrust - Audit Page (UC62)");
+		
+		//typing out I understand
+		driver.findElement(By.name("understand")).sendKeys("I UNDERSTAND");
+		driver.findElement(By.name("understand")).submit();
+		
+		//asserting activation
+		assertEquals("Patient Successfully Activated", driver.findElement(By.className("iTrustMessage")).getText());
+	}
+		
+	public void testHCPActivatePatientWrongConfirmation() throws Exception {
+		WebDriver driver = login("9000000000", "pw");
+		
+		driver.findElement(By.linkText("Audit Patients")).click();
+		assertEquals(driver.getTitle(), "iTrust - Please Select a Patient");
+		
+		//searching for patient 314159
+		driver.findElement(By.id("allowDeactivated")).click();
+		driver.findElement(By.name("UID_PATIENTID")).sendKeys("314159");
+		driver.findElement(By.name("UID_PATIENTID")).submit();
+		assertEquals(driver.getTitle(), "iTrust - Audit Page (UC62)");
+		
+		//typing out I understand
+		driver.findElement(By.name("understand")).sendKeys("iunderstand");
+		driver.findElement(By.name("understand")).submit();
+		
+		//asserting activation
+		assertEquals("You must type \"I UNDERSTAND\" in the textbox.", driver.findElement(By.className("iTrustError")).getText());
+	}
 }
